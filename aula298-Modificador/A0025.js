2c1872bc780ae5a134f37989fa9b425a9d64f2a3
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-01-13 17:09:17 -0300

    aula298 - Modificador e Protected

---
+++ aula298-Modificador/A0025.js
@@ -0,0 +1,48 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Colaborador = exports.Udemy = exports.Empresa = void 0;
+class Empresa {
+    constructor(nome, cnpj) {
+        this.colaboradores = [];
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
+    adicionaColaborador(colaborador) {
+        this.colaboradores.push(colaborador);
+    }
+    mostrarColaboradores() {
+        for (const colaborador of this.colaboradores) {
+            console.log(colaborador);
+        }
+    }
+}
+exports.Empresa = Empresa;
+class Udemy extends Empresa {
+    constructor() {
+        super("Udemy", "11.111.111/0001-11");
+    }
+    popColaborador() {
+        const colaborador = this.colaboradores.pop();
+        if (colaborador)
+            return colaborador;
+        return null;
+    }
+}
+exports.Udemy = Udemy;
+class Colaborador {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
+exports.Colaborador = Colaborador;
+const empresa1 = new Udemy();
+const colaborador1 = new Colaborador("Dimas", "Capelari");
+const colaborador2 = new Colaborador("Maria Luiza", "Capelari");
+const colaborador3 = new Colaborador("Mariana", "Capelari");
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+empresa1.adicionaColaborador(colaborador3);
+const colaboradorRemovido = empresa1.popColaborador();
+console.log(colaboradorRemovido);
+console.log(empresa1.nome);
