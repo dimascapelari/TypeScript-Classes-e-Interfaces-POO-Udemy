b5211603b506cf4603c88823c8cb4c704be889e4
Dimas Capelari <97951537+dimascapelari@users.noreply.github.com>
2023-01-17 11:02:33 -0300

    aula305 - Agregação entre classes

--- aula305-Agregacao-entre-Classes/A0031.js
+++ aula305-Agregacao-entre-Classes/A0031.js
@@ -5,17 +5,37 @@ class CarrinhoDeCompras {
     constructor() {
         this.produtos = [];
     }
+    inserirProduto(...produtos) {
+        for (const produto of produtos) {
+            this.produtos.push(produto);
+        }
+    }
+    quantidadeProdutos() {
+        return this.produtos.length;
+    }
+    valorTotal() {
+        let x = this.produtos.reduce((soma, produto) => soma + produto.preco, 0);
+        //   .toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+        //   .toFixed(2);
+        return x;
+        // return Math.round(x);
+    }
 }
 exports.CarrinhoDeCompras = CarrinhoDeCompras;
 class Produto {
-    constructor(_nome, preco) {
-        this._nome = _nome;
+    constructor(nome, preco) {
+        this.nome = nome;
         this.preco = preco;
-    }
-    get nome() {
-        return this._nome;
     }
 }
 exports.Produto = Produto;
 const produto1 = new Produto("Camiseta", 49.9);
-console.log(produto1.nome);
+const produto2 = new Produto("Caneca", 1.9);
+const produto3 = new Produto("Caneta", 0.9);
+const carrinhoDeCompras = new CarrinhoDeCompras();
+carrinhoDeCompras.inserirProduto(produto1, produto2, produto3);
+console.log(carrinhoDeCompras);
+console.log(carrinhoDeCompras.valorTotal());
+console.log(carrinhoDeCompras.quantidadeProdutos());
+// console.log(produto1.nome);
+// console.log(produto1.preco);
--- aula305-Agregacao-entre-Classes/A0031.ts
+++ aula305-Agregacao-entre-Classes/A0031.ts
@@ -1,14 +1,50 @@
 export class CarrinhoDeCompras {
   private readonly produtos: Produto[] = [];
-}
 
-export class Produto {
-  constructor(private _nome: string, public preco: number) {}
+  inserirProduto(...produtos: Produto[]): void {
+    for (const produto of produtos) {
+      this.produtos.push(produto);
+    }
+  }
 
-  get nome(): string {
-    return this._nome;
+  quantidadeProdutos(): number {
+    return this.produtos.length;
+  }
+
+  valorTotal(): number {
+    let x = this.produtos.reduce((soma, produto) => soma + produto.preco, 0);
+    //   .toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+    //   .toFixed(2);
+    return x;
+    // return Math.round(x);
   }
 }
 
+export class Produto {
+  constructor(public nome: string, public preco: number) {}
+
+  /*
+   constructor(private _nome: string, private preco: number) {}
+
+   get nome(): string {
+     return this._nome;
+   }
+  
+  get preco(): number {
+    return this._preco;
+  } 
+  */
+}
+
 const produto1 = new Produto("Camiseta", 49.9);
-console.log(produto1.nome);
+const produto2 = new Produto("Caneca", 1.9);
+const produto3 = new Produto("Caneta", 0.9);
+
+const carrinhoDeCompras = new CarrinhoDeCompras();
+carrinhoDeCompras.inserirProduto(produto1, produto2, produto3);
+console.log(carrinhoDeCompras);
+console.log(carrinhoDeCompras.valorTotal());
+console.log(carrinhoDeCompras.quantidadeProdutos());
+
+// console.log(produto1.nome);
+// console.log(produto1.preco);
